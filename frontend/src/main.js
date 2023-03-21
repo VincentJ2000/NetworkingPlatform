186c4c06f5208e0a1f10f5e50b65e2c24dcbc97f
Vincent Julio Jap <v.jap@student.unsw.edu.au>
2023-03-21 23:35:41 +1100

    view my profile done

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -50,7 +50,7 @@ document.getElementById('register-button').addEventListener('click', () => {
 
       apiCall('auth/register', 'POST', payload)
           .then((data) => {
-            setToken(data.token);
+            setToken(data.token, data.userId);
           });
     }
 });
@@ -76,7 +76,7 @@ document.getElementById('login-button').addEventListener('click', () => {
     }
     apiCall('auth/login', 'POST', payload)
         .then((data) => {
-          setToken(data.token);
+          setToken(data.token, data.userId);
         });
 });
 
@@ -188,6 +188,7 @@ const getUserData = (id) => {
   return new Promise((resolve, reject) => {
     apiCall('user', 'GET', {}, `?userId=${id}`)
       .then((data) => {
+        console.log(data);
         resolve(data);
       })
   });
@@ -218,6 +219,127 @@ const createCommentChild = (comment) => {
   return individualComment;
 }
 
+const getMyProfile = () => {
+  const id = localStorage.getItem('userId');
+  console.log(id);
+  getUserData(id)
+  .then((data) => {
+    
+      const inputId = document.querySelector('input[aria-label="id"]');
+      const inputEmail = document.querySelector('input[aria-label="email"]');
+      const inputName = document.querySelector('input[aria-label="name"]');
+      const inputWatching = document.querySelector('input[aria-label="watching"]');
+      
+      inputId.value = '';
+      inputEmail.value = '';
+      inputName.value = '';
+      inputWatching.value = '';
+
+      inputId.value = data.id;
+      inputEmail.value = data.email;
+      inputName.value = data.name;
+      inputWatching.value = `Watched by ${data.watcheeUserIds.length} people: `;
+    
+      if (data.watcheeUserIds.length === 1){
+        getUserData(data.watcheeUserIds[0]).then((data) => {
+          inputWatching.value += data.name;
+        });
+      }else if((data.watcheeUserIds.length > 1)){
+        getUserData(data.watcheeUserIds[0]).then((data) => {
+          inputWatching.value += data.name;
+        });
+        for (let i = 1; i < data.watcheeUserIds.length; i++){
+          getUserData(data.watcheeUserIds[i]).then((data) => {
+            inputWatching.value += ", " + data.name;
+          });
+        }
+      }
+      
+      const jobs = data.jobs;
+      document.getElementById('job-list').textContent = '';
+      for (const jobItem of jobs) {
+          console.log(jobItem)
+
+          const card = document.createElement("div");
+          card.classList.add("card");
+          card.classList.add("mb-3");
+
+          const cardImage = document.createElement("img");
+          cardImage.classList.add("card-img-top");
+          cardImage.setAttribute("src", jobItem.image);
+          cardImage.setAttribute("alt", "Job Image");
+          cardImage.setAttribute("style", "max-width: 100%; height: 200px;");
+
+          const cardBody = document.createElement("div");
+          cardBody.classList.add("card-body");
+
+          const cardTitle = document.createElement("h3");
+          cardTitle.classList.add("card-title");
+          cardTitle.textContent = jobItem.title;
+
+          const cardText1 = document.createElement("p");
+          cardText1.classList.add("card-text");
+          cardText1.textContent = jobItem.description;
+
+          const cardText4 = document.createElement("p");
+          cardText4.classList.add("card-text");
+          cardText4.textContent = "Start Date: " + jobItem.start.substring(0,10);
+
+          const cardText3 = document.createElement("p");
+          cardText3.classList.add("card-text");
+          getUserData(jobItem.creatorId).then((data) => {
+            cardText3.textContent = "Creator: " + data.name;
+          })
+          
+          const cardText2 = document.createElement("p");
+          cardText2.classList.add("card-text");
+
+          const cardText2Small = document.createElement("small");
+          cardText2Small.classList.add("text-muted");
+          cardText2Small.textContent = getDate(jobItem.createdAt);
+
+          cardText2.appendChild(cardText2Small);
+          cardBody.appendChild(cardTitle);
+          cardBody.appendChild(cardText4);
+          cardBody.appendChild(cardText1);
+          cardBody.appendChild(cardText3);
+          cardBody.appendChild(cardText2);
+          card.appendChild(cardImage);
+          card.appendChild(cardBody);
+          document.getElementById('job-list').appendChild(card);
+      }
+  });
+}
+
+const navigateTab = () => {
+  const navigationLink = document.querySelectorAll('.nav-item.nav-link');
+  navigationLink.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetTab = document.querySelector(event.target.dataset.target);
+      const targetLink = event.target.getAttribute('id');
+      
+      document.querySelector('.nav-item.nav-link.active').classList.remove('active');
+      document.querySelector('.tab-pane.fade.show.active').classList.remove('show', 'active');
+      event.target.classList.add('active');
+      targetTab.classList.add('show', 'active');
+
+      navigationLink.forEach((link) => {
+        if (link.getAttribute('id') === targetLink) {
+          link.setAttribute('aria-selected', true);
+        } else {
+          link.setAttribute('aria-selected', false);
+        }
+      });
+
+      //call get my profile here
+      if(targetLink === "nav-profile-tab"){
+        getMyProfile();
+      }
+    });
+  });
+}
+
 
 //helper functions
 const show = (element) => {
@@ -226,11 +348,13 @@ const show = (element) => {
 const hide = (element) => {
     document.getElementById(element).classList.add('hide');
 }
-const setToken = (token) => {
+const setToken = (token, id) => {
     localStorage.setItem('token', token);
+    localStorage.setItem('userId', id);
     show('section-logged-in');
     hide('section-logged-out');
     populateFeed();
+    navigateTab();
 }
 const getDate = (dateString) => {
     const givenDate = new Date(dateString);
@@ -269,6 +393,7 @@ document.getElementById('logout').addEventListener('click', () => {
     show('section-logged-out');
     hide('section-logged-in');
     localStorage.removeItem('token');
+    localStorage.removeItem('userId');
 });
 
 
@@ -277,5 +402,6 @@ if (localStorage.getItem('token')) {
     show('section-logged-in');
     hide('section-logged-out');
     populateFeed();
+    navigateTab();
 }
   
